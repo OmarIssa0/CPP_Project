a2806f31854518c145b08d18935f6bf9ba88cb92
OmarIssa0 <omar.issa.slwade@gmail.com>
2025-10-05 20:14:41 +0300

    cpp 08

--- CPP_Module08/ex02/MutantStack.hpp
+++ CPP_Module08/ex02/MutantStack.hpp
@@ -3,15 +3,32 @@
 
 #include <string>
 #include <iostream>
+#include <stack>
+#include <deque>
 
 template <typename T>
 class MutantStack : public std::stack<T>
 {
     public:
-        MutantStack(int);
-        MutantStack(const MutantStack &other);
-        MutantStack &operator=(const MutantStack &other);
-        ~MutantStack();
-}
+        MutantStack()  {}
+        MutantStack(const MutantStack &other) : std::stack<T>(other) {}
+        MutantStack &operator=(const MutantStack &other)
+        {
+            std::stack<T>::operator=(other);
+            return *this;
+        }
+        ~MutantStack() {};
+
+        typedef typename std::stack<T>::container_type::iterator iterator;
+
+        iterator begin()
+        {
+            return this->c.begin();
+        }
+        iterator end()
+        {
+            return this->c.end();
+        }
+};
 
 #endif
